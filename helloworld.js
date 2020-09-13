6a0fb30a3d3f8f2aef4bb0bc99722a6a193db20e
Mike Colbert <mcolbert1@live.maryville.edu>
2020-09-13 15:32:59 -0500

    updated to include week and assignment for Ansible

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Hello Professor Sindel\n it\'s Mike Colbert\'s page\n')
+   response.end('Hello Professor Sindel\n it\'s Mike Colbert\'s page\n \n this is my week 3 Ansible assignment')
 }).listen(3333)
 
 // Console will print the message
